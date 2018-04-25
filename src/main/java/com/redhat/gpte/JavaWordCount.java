3cea304e0de99ac0e7ef77045a75bb6da0b0c7ef
Pkrish15 <35951257+Pkrish15@users.noreply.github.com>
2018-04-25 13:15:24 +0530

    Update JavaWordCount.java

--- src/main/java/com/redhat/gpte/JavaWordCount.java
+++ src/main/java/com/redhat/gpte/JavaWordCount.java
@@ -15,11 +15,11 @@ import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 public class JavaWordCount {
 	public static void main(String[] args) throws Exception {
-		String inputFile = "src/main/resources/input.txt";
+		//String inputFile = "src/main/resources/input.txt";
 		//Initialize Spark Context
 		JavaSparkContext sc = new JavaSparkContext(new SparkConf().setAppName("wordCount").setMaster("local[*]"));
-		// Load data from Input File.
-		JavaRDD<String> input = sc.textFile(inputFile);
+		// Load data from Input File. Generic Input Directory not from IDE Perspective, In case of Hadoop Platform, Please use this below.
+		JavaRDD<String> input = sc.textFile("/tmp/input.txt");
 		// Split up into words.
 		JavaPairRDD<String, Integer> counts = input.flatMap(line -> Arrays.asList(line.split(" ")).iterator())
 				.mapToPair(word -> new Tuple2<>(word, 1)).reduceByKey((a, b) -> a + b);
